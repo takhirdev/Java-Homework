a29cec3f8b52157d9c60591ad779268e8daee592
Takhir <takhir.ismailoff@gmail.com>
2023-10-27 21:31:42 +0500

    added java files

---
+++ Polymorphism (Overloading)/MathFuntions.java
@@ -0,0 +1,61 @@
+public class MathFuntions {
+    int add(int a, int b){
+        return a + b;
+    }
+    double add(int a, double b){
+        return a + b;
+    }
+    double add(double a, int b){
+        return a + b;
+    }
+    double add(double a, double b){
+        return a + b;
+    }
+    int sub (int a, int b){
+        return a - b;
+    }
+    double sub (double a, int b){
+        return a - b;
+    }
+    double sub (int a, double b){
+        return a - b;
+    }
+    double sub (double a, double b){
+        return a - b;
+    }
+    int multiply(int a, int b){
+        return a * b;
+    }
+    double multiply(double a, int b){
+        return a * b;
+    }
+    double multiply(int a, double b){
+        return a * b;
+    }double multiply(double a, double b){
+        return a * b;
+    }
+    int div(int a, int b){
+        return a / b;
+    }
+    double div(double a, int b){
+        return a / b;
+    }
+    double div(int a, double b){
+        return a / b;
+    }
+    double div(double a, double b){
+        return a / b;
+    }
+   int abs(int a){
+        return Math.abs(a);
+   }
+    double abs(double a){
+        return Math.abs(a);
+    }
+   int pow(int a){
+        return (int) Math.pow(a,2);
+    }
+    double pow(double a){
+        return Math.pow(a,2);
+    }
+}
